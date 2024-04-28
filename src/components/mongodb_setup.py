b2de3640f01d50c01095baedafd0ad8d66543676
Nathan Gabriel <nategabrielk@icloud.com>
2024-04-28 14:01:28 -0400

    added comoponent that uploads meta data to mongo db

---
+++ src/components/mongodb_setup.py
@@ -0,0 +1,43 @@
+import os, sys
+
+from src.utils.database_handler import MongodbClient
+from src.exeception import CustomException
+from src.logger import logging
+
+class MetaDataStore:
+
+    def __init__(self):
+        self.root = os.getcwd()
+        self.images = os.path.join(self.root, "caltech-101")
+        self.labels = os.listdir(self.images)
+        self.mongo = MongodbClient()
+    
+    def register_labels(self):
+        try:
+            records = {}
+            for num, label in enumerate(self.labels):
+                records[f"{num}"] = label
+
+            self.mongo.database['labels'].insert_one(records)
+            logging.info("Created")
+            return {"Created", True}
+        except Exception as e:
+            error = CustomException(e, sys)
+            logging.error("Could not create")
+            return {"Created": False, "Reason": error.error_message}
+    
+    def run_step(self):
+        try:
+            self.register_labels()
+            logging.info("Created")
+            return {"Created", True}
+        except Exception as e:
+            error = CustomException(e, sys)
+            logging.error("Could not create")
+            return {"Created": False, "Reason": error.error_message}
+
+
+if __name__ == "__main__":
+    meta = MetaDataStore()
+    meta.run_step()
+
